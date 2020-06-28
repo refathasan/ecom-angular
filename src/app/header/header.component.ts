e656931386eeed17d10d2378411e303d03736bcf
refat hasan <refat.gononet@hotmail.com>
2020-06-28 18:29:42 +0600

    template_making_commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css','./header.component.mobile.css',
+  './header.component.csss.css','./header.component.bootstrap.css','./header.component.slick.css',
+'./header.component.font_awsom.css','./header.component.39032300.css','./header.component.032b6fde.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
